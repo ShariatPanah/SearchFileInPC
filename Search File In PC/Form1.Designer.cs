e9dbc80ec3f2c863cb6a3de77189c66da4adf5cd
Sirwan ShariatPanah <er.sirwan@live.com>
2023-08-23 13:16:05 +0330

    Add project files.

---
+++ Search File In PC/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Search_File_In_PC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFileName = new System.Windows.Forms.TextBox();
+            this.lstFilteredFiles = new System.Windows.Forms.ListBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Location = new System.Drawing.Point(244, 31);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.Size = new System.Drawing.Size(408, 20);
+            this.txtFileName.TabIndex = 0;
+            // 
+            // lstFilteredFiles
+            // 
+            this.lstFilteredFiles.FormattingEnabled = true;
+            this.lstFilteredFiles.Location = new System.Drawing.Point(12, 72);
+            this.lstFilteredFiles.Name = "lstFilteredFiles";
+            this.lstFilteredFiles.Size = new System.Drawing.Size(730, 550);
+            this.lstFilteredFiles.TabIndex = 2;
+            this.lstFilteredFiles.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.lstFilteredFiles_MouseDoubleClick);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(140, 29);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 1;
+            this.btnSearch.Text = "جستجو";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(754, 635);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.lstFilteredFiles);
+            this.Controls.Add(this.txtFileName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFileName;
+        private System.Windows.Forms.ListBox lstFilteredFiles;
+        private System.Windows.Forms.Button btnSearch;
+    }
+}
+
---
+++ Search File In PC/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Search_File_In_PC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            Searching(txtFileName.Text);
+        }
+
+        public void Searching(string fileName)
+        {
+            foreach (string s in Directory.GetLogicalDrives())
+            {
+                var path = @"C:\Users\ersir\Downloads\Music";
+                foreach (string file in Directory.EnumerateFiles(s))
+                {
+                    try
+                    {
+                        if (file.ToLower().Contains(fileName))
+                        {
+                            lstFilteredFiles.Items.Add(file);
+                        }
+                    }
+                    catch (System.Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+            }
+        }
+
+        private void lstFilteredFiles_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (lstFilteredFiles.SelectedItems.Count != 0)
+            {
+                Process.Start(lstFilteredFiles.SelectedItems[0].ToString());
+            }
+        }
+    }
+}
